31733f8a494aac6338b5783e74d8d0e1bda7e0b7
Jose Maria Ros <josemariarosnicolas00@gmail.com>
2024-08-19 16:20:05 +0200

    Ansible environment preparation

---
+++ ansible-environment-prep.sh
@@ -0,0 +1,9 @@
+sudo apt update
+
+sudo apt install software-porpierties-common
+
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+
+sudo apt install ansible
+
+ansible-galaxy collection install ansible.windows
