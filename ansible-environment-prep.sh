21c527ad14e565bd778432e0d38d18131b95b482
Jose Maria Ros <josemariarosnicolas00@gmail.com>
2024-08-20 13:35:22 +0200

    Update ansible-environment-prep.sh

--- ansible-environment-prep.sh
+++ ansible-environment-prep.sh
@@ -1,6 +1,7 @@
+ #!/bin/bash
 sudo apt update
 
-sudo apt install software-porpierties-common
+sudo apt install software-propiertis-common
 
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 
